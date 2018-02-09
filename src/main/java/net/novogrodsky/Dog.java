62df1b570e1ebcb0437b3db5ce18a6f190d6ca79
David Novogrodsky <david.novogrodsky@gmail.com>
2018-02-08 21:09:51 -0600

    experiments with inheritance -- DJN

---
+++ src/main/java/net/novogrodsky/Dog.java
@@ -0,0 +1,17 @@
+package net.novogrodsky;
+
+/**
+ *
+ */
+public class Dog extends Animal {
+
+  public Dog() {
+    this(10, 15, 12, 13);
+  }
+
+  public Dog(int brain, int body, int size, int weight) {
+    super(brain, body, size, weight);
+  }
+
+
+}
---
+++ src/test/java/net/novogrodsky/DogTest.java
@@ -0,0 +1,17 @@
+package net.novogrodsky;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class DogTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
