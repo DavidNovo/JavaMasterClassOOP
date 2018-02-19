e189cf220386dc23b1b1635caf3fe0dfd3eb1c1c
David Novogrodsky <david.novogrodsky@gmail.com>
2018-02-18 20:08:57 -0600

    experiments with inheritance -- DJN

---
+++ src/test/java/net/novogrodsky/FishTest.java
@@ -0,0 +1,18 @@
+package net.novogrodsky;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class FishTest {
+
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
